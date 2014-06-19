ed244c9dbc46864f5a27e7de24881a8252901621
kreamt <karimtabet@gmail.com>
2014-06-19 23:11:02 +0100

    removes pure css, plugs in font awesome, styles github page

--- js/jquery.generate-github-data.js
+++ js/jquery.generate-github-data.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
     $.getJSON('js/github-data.json', function(data) {
         $.each(data, function(key, val) {
-            $('#github_repos').append('<div><a href="' + val.html_url + '">' + val.full_name + '</a><div>')
+            $('#github_repos').append(
+                '<div class="repo_forks">' 
+                + '<a href="' 
+                + val.forks_url + '"><i class="fa fa-code-fork"></i> ' 
+                + val.forks_count + '</a></div>'
+                +'<div class="repo_name"><a href="' 
+                + val.html_url + '">' 
+                + val.full_name + '</a><div>'
+            );
         });
     });
 });
+
+            
