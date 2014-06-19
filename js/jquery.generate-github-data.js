86227485d0a3ef9eebf50225533c5e14322f511f
kreamt <karimtabet@gmail.com>
2014-06-19 23:36:14 +0100

    styling github page

--- js/jquery.generate-github-data.js
+++ js/jquery.generate-github-data.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
     $.getJSON('js/github-data.json', function(data) {
         $.each(data, function(key, val) {
+            var forks_url = 'https://github.com/'
+            + val.full_name + '/network';
             $('#github_repos').append(
                 '<div class="repo_forks">' 
-                + '<a href="' 
-                + val.forks_url + '"><i class="fa fa-code-fork"></i> ' 
+                + '<a title="Forks" href="' 
+                + forks_url + '" target="_blank">'
+                + '<i class="fa fa-code-fork"></i> ' 
                 + val.forks_count + '</a></div>'
                 +'<div class="repo_name"><a href="' 
-                + val.html_url + '">' 
+                + val.html_url + '" target="_blank">' 
                 + val.full_name + '</a><div>'
             );
         });
