3e2efdb4a9723308e28babd519e7243c19051c6f
kreamt <karimtabet@gmail.com>
2014-06-20 01:16:08 +0100

    adds repo description and removes subdomain from repo names

--- js/jquery.generate-github-data.js
+++ js/jquery.generate-github-data.js
@@ -4,14 +4,20 @@ $(document).ready(function() {
             var forks_url = 'https://github.com/'
             + val.full_name + '/network';
             $('#github_repos').append(
+                //Forks
                 '<div class="repo_forks">' 
                 + '<a title="Forks" href="' 
                 + forks_url + '" target="_blank">'
                 + '<i class="fa fa-code-fork"></i> ' 
                 + val.forks_count + '</a></div>'
+                //Full name
                 +'<div class="repo_name"><a href="' 
                 + val.html_url + '" target="_blank">' 
-                + val.full_name + '</a><div>'
+                + val.full_name.replace('karimtabet/', '') 
+                + '</a><div>'
+                //Description
+                +'<div class="repo_description">'
+                + val.description + '<div>'
             );
         });
     });
