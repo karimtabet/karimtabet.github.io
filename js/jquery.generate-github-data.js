09083cee82cdd135ab7ebeac755ba4bb8ac5ea51
kreamt <karimtabet@gmail.com>
2014-06-20 01:44:18 +0100

    pulling repo language, styling github page

--- js/jquery.generate-github-data.js
+++ js/jquery.generate-github-data.js
@@ -5,10 +5,11 @@ $(document).ready(function() {
             + val.full_name + '/network';
             $('#github_repos').append(
                 //Forks
-                '<div class="repo_forks">' 
-                + '<a title="Forks" href="' 
+                '<div class="repo_stats">'
+                + val.language
+                + ' <a title="Forks" href="' 
                 + forks_url + '" target="_blank">'
-                + '<i class="fa fa-code-fork"></i> ' 
+                + '<i class="fa fa-code-fork"></i>' 
                 + val.forks_count + '</a></div>'
                 //Full name
                 +'<div class="repo_name"><a href="' 
