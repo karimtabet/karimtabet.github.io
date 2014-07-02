634e706d4c997079ebcdc9725b784be426d129d9
kreamt <karimtabet@gmail.com>
2014-07-02 14:14:37 +0100

    writing swf controller

---
+++ js/angular-app/controllers/swfController.js
@@ -0,0 +1,22 @@
+app.controller("swfController", function($scope, $http) {
+    console.log(getAllFilesFromFolder('../../)'));
+});
+
+var getAllFilesFromFolder = function(dir) {
+
+    var filesystem = require("fs");
+    var results = [];
+
+    filesystem.readdirSync(dir).forEach(function(file) {
+
+        file = dir+'/'+file;
+        var stat = filesystem.statSync(file);
+
+        if (stat && stat.isDirectory()) {
+            results = results.concat(_getAllFilesFromFolder(file))
+        } else results.push(file);
+
+    });
+
+    return results;
+};
